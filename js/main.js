91ed5a74a27adeab19227e71a90a095decfc93b1
Your Name <kyawtkyawtsan1998@gmail.com>
2021-08-14 14:35:36 +0630

    adding

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector('.hamburger')
+const hamburgerMenu = document.querySelector('.nav')
+hamburger.addEventListener('click', () =>{
+    hamburger.classList.toggle('active')
+    hamburgerMenu.classList.toggle('active')
+})
+
+
+
+
+
+
+
