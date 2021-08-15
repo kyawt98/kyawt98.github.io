0d3ea6e940088339c1e665b6f0578a4236ca1843
Your Name <kyawtkyawtsan1998@gmail.com>
2021-08-15 11:49:18 +0630

    update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const hamburger = document.querySelector('.hamburger')
-const hamburgerMenu = document.querySelector('.nav')
+const hamburgerMenu = document.querySelector('.nav-container')
 hamburger.addEventListener('click', () =>{
     hamburger.classList.toggle('active')
     hamburgerMenu.classList.toggle('active')
